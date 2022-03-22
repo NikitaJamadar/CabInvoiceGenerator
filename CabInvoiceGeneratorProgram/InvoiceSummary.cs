77f3f760d601c66f8e7be29c3b6d1d16657b315a
NikitaJamadar <nikhitajamadar1998@gmail.com>
2022-03-22 09:26:26 +0530

    CAb Invoice Generator program

---
+++ CabInvoiceGeneratorProgram/InvoiceSummary.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGeneratorProgram
+{
+    public class InvoiceSummary
+    {
+        public int numberOfRides;
+        public double totalFare;
+        public double averageFare;
+
+        public InvoiceSummary(int numberOfRides, double totalFare)
+        {
+            this.numberOfRides = numberOfRides;
+            this.totalFare = totalFare;
+            this.averageFare = this.totalFare / this.numberOfRides;
+        }
+    }
+}
---
+++ CabInvoiceGeneratorProgram/RideType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CabInvoiceGeneratorProgram
+{
+   
+        public enum RideType
+        {
+            NORMAL,
+            PREMIUM
+
+        }
+   
+}
